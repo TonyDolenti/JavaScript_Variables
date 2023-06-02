bddb46c2bdb5dd300f63486e37d33e8e31eb3d1f
TonyDolenti <antoniobdolenti@gmail.com>
2023-06-02 00:14:16 -0400

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ console.log(
 
 // Exercise 1
 
+let lastName;
+var age;
 // Exercise 2
-
+const firstName = "Bruce";
+lastName = "Wayne";
+age = 20;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
